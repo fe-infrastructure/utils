bd8ceab8cf66b3f993c8789d207b31b24e9cd7fd
yanquanfahei <1449826851@qq.com>
2023-04-09 18:33:17 +0800

    test: parse test

---
+++ test/url/parse.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from 'vitest'
+
+import { parse } from '@/url/parse'
+
+test('parse url protocol', () => {
+  expect(parse('https://foo')).toMatchInlineSnapshot(`
+    {
+      "protocol": "https:",
+    }
+  `)
+})
