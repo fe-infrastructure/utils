fc9f39510a5f6e89708be0678189fa2615f95fe0
yanquanfahei <1449826851@qq.com>
2023-04-10 10:03:24 +0800

    test: parse url

--- test/url/parse.test.ts
+++ test/url/parse.test.ts
@@ -2,10 +2,32 @@ import { test, expect } from 'vitest'
 
 import { parse } from '@/url/parse'
 
-test('parse url protocol', () => {
-  expect(parse('https://foo')).toMatchInlineSnapshot(`
+test('parse history url', () => {
+  expect(parse('https://www.baidu.com/foo/bar/?foo=foo&bar=bar')).toMatchInlineSnapshot(`
     {
+      "hash": "",
+      "host": "www.baidu.com",
+      "hostname": "www.baidu.com",
+      "href": "https://www.baidu.com/foo/bar/?foo=foo&bar=bar",
+      "pathname": "/foo/bar/",
+      "port": "",
       "protocol": "https:",
+      "search": "?foo=foo&bar=bar",
+    }
+  `)
+})
+
+test('parse hash url', () => {
+  expect(parse('http://google.com/parse-things/?bar=bar#/path?foo=foo')).toMatchInlineSnapshot(`
+    {
+      "hash": "#/path?foo=foo",
+      "host": "google.com",
+      "hostname": "google.com",
+      "href": "http://google.com/parse-things/?bar=bar#/path?foo=foo",
+      "pathname": "/parse-things/",
+      "port": "",
+      "protocol": "http:",
+      "search": "?bar=bar",
     }
   `)
 })
